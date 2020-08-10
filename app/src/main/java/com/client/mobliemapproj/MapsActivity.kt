d2d9891dd97b64c0b8c4b8dc4e45f1f85822bc43
jinkyo <>
2020-08-10 09:39:45 +0900

    Geocoding을 이용한 mark기능 추가 https://github.com/jinkyo422/MoblieMapProj/issues/1

--- app/src/main/java/com/client/mobliemapproj/MapsActivity.kt
+++ app/src/main/java/com/client/mobliemapproj/MapsActivity.kt
@@ -1,7 +1,9 @@
 package com.client.mobliemapproj
 
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +11,16 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var geocoder: Geocoder
+    private var store = mutableListOf("서울시청", "국회의사당", "서울 서초구 신반포로 194", "아라마크현대카드캐피탈여의도본사")
+    private var zoomLevel = 12F
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +33,53 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        geocoder = Geocoder(this)
 
         val seoul = LatLng(37.5217, 126.9243)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 12F))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, zoomLevel))
         mMap.addMarker(MarkerOptions().position(seoul).title("Marker in Seoul"))
+        var zoom = seoul
+
+        zoomPlus.setOnClickListener {
+
+            if (zoomLevel != 21F) {
+                zoomLevel += 1F
+            }
+            println(zoomLevel)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoom, zoomLevel))
+        }
+        zoomMinus.setOnClickListener {
+
+            if (zoomLevel != 2F) {
+                zoomLevel -= 1F
+            }
+            println(zoomLevel)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoom, zoomLevel))
+        }
+        next.setOnClickListener {
+
+            if (index == store.size) {
+                Toast.makeText(this, "마지막입니다", Toast.LENGTH_SHORT).show()
+            } else {
+                try {
+                    println(store[index])
+                    val address = geocoder.getFromLocationName(store[index], 10)
+                    println(address.toString())
+
+                    val latitude = address[0].latitude
+                    val longitude = address[0].longitude
+
+                    val new = LatLng(latitude, longitude)
+                    mMap.addMarker(MarkerOptions().position(new).title(store[index]))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new, zoomLevel))
+
+                    zoom = new
+                    index++
+
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
