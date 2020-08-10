1601f190408606cb926be40f47ba29840e4753ef
jinkyo <>
2020-08-10 09:32:49 +0900

    Google Map Api 연동 https://github.com/jinkyo422/MoblieMapProj/issues/1

---
+++ app/src/main/java/com/client/mobliemapproj/MapsActivity.kt
@@ -0,0 +1,33 @@
+package com.client.mobliemapproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val seoul = LatLng(37.5217, 126.9243)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 12F))
+        mMap.addMarker(MarkerOptions().position(seoul).title("Marker in Seoul"))
+    }
+}
